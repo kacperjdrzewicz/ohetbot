93e30b993f2e7e46b9c0fe4bd06d95be462f50af
kacperjdrzewicz <kacperjdrzewicz@gmail.com>
2017-12-14 20:07:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '.OHETEasy') {
+    if (message.content === '.OHETE') { //Łatwy Rzut
         var rzut1 = Math.floor((Math.random()*100))%6;
         var rzut2 = Math.floor((Math.random()*100))%6;
         var tekst = "\nTwoje rzuty: \"" + wyniki[rzut1] + "\" oraz \"" + wyniki[rzut2] + "\"\n";
@@ -21,12 +21,12 @@ client.on('message', message => {
         }
         message.reply(tekst);
     }
-    else if(message.content === '.OHETMedium') {
+    else if(message.content === '.OHETM') { //Przeciętny Rzut
         var rzut = Math.floor((Math.random()*100))%6;
         var tekst = "\nRezultat: \"" + wyniki[rzut]+ "\"";
         message.reply(tekst);
     }
-    else if(message.content === '.OHETHard') {
+    else if(message.content === '.OHETH') { //Trudny Rzut
         var rzut1 = Math.floor((Math.random()*100))%6;
         var rzut2 = Math.floor((Math.random()*100))%6;
         var tekst = "\nTwoje rzuty: \"" + wyniki[rzut1] + "\" oraz \"" + wyniki[rzut2] + "\"\n";
@@ -36,6 +36,46 @@ client.on('message', message => {
         else {
             tekst += "Rezultat: \"" + wyniki[rzut1] + "\"";
         }
+        message.reply(tekst);
+    }
+    else if(message.content === '.OHETR') { //Tabela Reakcji
+        var rzut1 = (Math.floor((Math.random()*100))%6) + 1;
+        var rzut2 = (Math.floor((Math.random()*100))%6) + 1;
+        var tekst = "\nSuma rzutów: " + (rzut1+rzut2) + " (" + rzut1 + " + " + rzut2 + ")\n";
+        switch(rzut1+rzut2) {
+            case 2:
+                tekst += "Rezultat: " + reakcje[0];
+            break;
+            case 3:
+                tekst += "Rezultat: " + reakcje[1];
+            break;
+            case 4:
+            case 5:
+                tekst += "Rezultat: " + reakcje[2];
+            break;
+            case 6:
+            case 7:
+            case 8:
+                tekst += "Rezultat: " + reakcje[3];
+            break;
+            case 9:
+            case 10:
+                tekst += "Rezultat: " + reakcje[4];
+            break;
+            case 11:
+                tekst += "Rezultat: " + reakcje[5];
+            break;
+            case 12:
+                tekst += "Rezultat: " + reakcje[6];
+        }
+        message.reply(tekst);
+    }
+
+    else if(message.content === '.OHETT') { //Tabela Pułapek
+        var rzut1 = Math.floor((Math.random()*100))%6;
+        var rzut2 = Math.floor((Math.random()*100))%6;
+        var tekst = "\nWiersz: " + (rzut1+1) + ", Kolumna: " + (rzut2+1) + "\n";
+        tekst += "Rezultat: " + pulapki[rzut1][rzut2];
         message.reply(tekst);
     }
 });
